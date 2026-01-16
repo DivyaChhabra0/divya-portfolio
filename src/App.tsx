b0b030a7425d1236e9b69b3095f7af2761ba6f10
ri8thik <rithiksoun88@gmail.com>
2026-01-16 22:39:53 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>i hate Rithik</h1>
       <h1>i hate me</h1>
-     
     </>
   )
 }
